dd7aec03c0390e69051447a094ede3b2d222afc9
Silvia Traversa <s.traversa98@gmail.com>
2020-04-29 18:35:08 +0200

    QT - upload picture and set parameters

--- dct2/mainwindow.cpp
+++ dct2/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QImage>
+#include <QPixmap>
+#include <QLabel>
+#include <QIntValidator>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +20,28 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString fname = QFileDialog::getOpenFileName(this, tr("Select image"), "/git/dct2", tr("Image Files (*.bmp)"));
+
+    QPixmap pixmap = QPixmap(fname);
+
+    ui->img->setPixmap(QPixmap(pixmap)); //Verifico caricamento immagine visualizzandola
+
+}
+
+void MainWindow::on_parameters_clicked()
+{
+    ui->writeF->setValidator(new QIntValidator());
+    int F = ui->writeF->text().toInt();
+    ui->writeD->setValidator(new QIntValidator());
+    int d = 0;
+    if (ui->writeD->text().toInt() > 0 && ui->writeD->text().toInt() < (2*F - 2) ){
+        d = ui->writeD->text().toInt();
+    }else {
+        QMessageBox msgBox;
+        msgBox.setText("Parametro non valido");
+        msgBox.exec();
+    }
+}
